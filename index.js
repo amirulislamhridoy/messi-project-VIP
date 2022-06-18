f4bbb5d13c0d4c5a26225b5281bfb65667efac97
Your Name <hossainhridoy@30687@gmail.com>
2022-06-19 02:25:44 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// menu and navbar inside menu
+const menuNavbar = document.getElementById('menu-navbar')
+document.body.addEventListener('click', function(e){
+    // console.log(e.target.parentNode.tagName)
+    const tar = e.target
+    if(tar.tagName.toLowerCase() !== 'div'){
+        menuNavbar.style.display = 'block'
+    }
+    else if(tar.tagName.toLowerCase() !== 'ul' || tar.parentNode.tagName.toLowerCase() !== 'ul' || tar.parentNode.parentNode.tagName.toLowerCase() !== 'ul'){
+        menuNavbar.style.display = 'none'
+        
+    }
+})
+
+const menu = () => {
+    menuNavbar.style.display = 'inline-block'
+}
